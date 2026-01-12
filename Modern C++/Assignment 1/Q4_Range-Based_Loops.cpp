7c15663535c169c6c8d0e18de795b5332cedbe51
janaa-wael <janawael119@gmail.com>
2026-01-12 09:55:46 +0200

    Q4: Range-Based loops in Modern C++

---
+++ Modern C++/Assignment 1/Q4_Range-Based_Loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    vector<int> v{1,2,3,4,5,6};
+    for(auto& c : v)
+    {
+        c *= 2;
+    }
+    for(auto c : v)
+    {
+        cout << c << " ";
+    }
+    cout << endl;
+}
